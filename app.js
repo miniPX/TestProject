61fc875ee7a997f2576f6f6d25aab86b6ded4b0b
Noel Eom <noeleom@gmail.com>
2018-09-18 15:39:54 -0700

    Front end side for Serena Christmas Present

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-var express = require('express'); 
+var express = require('express'),
+    app = express(),
+    port = process.env.port || 3001; 
+
+app.listen(port, () => {
+    console.log('Server listening on port 3001');
+});
+
+// app.get('/test', function(req, res) {
+//     return res.json({'success': 'true'});
+//   });
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/views/index.html'); 
+}); 
